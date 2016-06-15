bcfda32271b11bb8fb82199096bbb0d8b31165a5
Hugo Henrique <contato@hugohenrique.com.br>
2016-06-15 16:03:14 -0300

    ...

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Table          from './table';
+import TableData      from './table-data';
+import TableDataMixin from './table-data-mixin';
+import DataSource     from './table-data-source';
+import Pagination     from './pagination';
+import SearchField    from './search-field';
+
+export {
+  Table,
+  TableData,
+  DataSource,
+  Pagination,
+  SearchField
+};
+
